ec57981c009d457738a0b4c7f356848c06614c03
revenoirr <federico.gym36@gmail.com>
2026-01-26 14:00:03 +0200

    11111

--- lab3-hotel-booking/src/state/BookingState.ts
+++
@@ -1,9 +0,0 @@
-import { Booking } from '../entities/Booking';
-
-export abstract class BookingState {
-  abstract confirm(booking: Booking): void;
-  abstract checkIn(booking: Booking): void;
-  abstract checkOut(booking: Booking): void;
-  abstract cancel(booking: Booking): void;
-  abstract getStatus(): string;
-}
